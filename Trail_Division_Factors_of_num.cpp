805c4147e74b87e157743500b1ca4cd5466d810b
RNoulia <36097160+RahulNoulia@users.noreply.github.com>
2020-04-19 14:28:15 +0530

    Update Trail_Division_Factors_of_num.cpp

--- Trail_Division_Factors_of_num.cpp
+++ Trail_Division_Factors_of_num.cpp
@@ -1,47 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+#define fo(i,n) for(i=0;i<n;i++) 
+#define foi(i,sq) for(i=1;i<=sq;i++)   
+#define ll  long long int
+typedef vector<ll>  vec;
+vec vi;
 int main(int argc, char const *argv[])
 {
-    int number,k=0;
+    ll number,sq;
+    int k=0,i;
     cout<<"\nEnter number : ";
     cin>>number;
-    int size = sqrt(number) * 2,sq = sqrt(number);
-    int arr[size]={};
-    for(int i=1;i<=sq;i++)                                          // Trail division method                                    
-      {
-         if(number%i==0)
-         {
-               arr[k]=i;
-               k++;
-               if(i!=sqrt(number))
-                {
-		       arr[k]=number/i;
-		       k++;
-                }
-         }
-      }                                                                        // end of trail division method
-       if(k>2)
-        {
-		    for(int i=0;i<k;i++)                                       //  Sorting for arrange the element in particular order
-		    {
-                         for(int j=i+1;j<k;j++)
-                             {
-			        if(arr[i]>arr[j])
-                                          arr[j]=(arr[i]+arr[j]) - (arr[i]=arr[j]);
-                              }
-		    }
-                    cout<<"\nFactors are :  ";
-                     for(int i=0;i<k;i++)                                       // All factors of number in a increasing order 
-		    {
-			 cout<<arr[i]<<" ";
-		    }
-        }
-      else
-       {
-   
-              cout<<"\nThe "<<number<<" is prime number ";                       // print number if it is prime                 
-       }
-        cout<<"\n\n";
+    sq = sqrt(number);
+   // Trail division method     
+    foi(i,sq){
+         if(number%i==0){
+               vi.push_back(i); k++;
+               if(i!=sq)   vi.push_back(number/i); k++;
+           }
+      }  
+    // end of trail division method 
+      if(k > 2 ){                                                                
+    	  sort(vi.begin(),vi.end());
+    	  cout<<"Factors are : ";
+    	  fo(i,vi.size()-1){  
+                    cout<<vi[i];
+                    if(i != vi.size()-2)   cout<<" , ";
+          }
+      }
+      else  cout<<"Number is Prime ";
+      cout<<"\n\n";
       return 0;
 }
